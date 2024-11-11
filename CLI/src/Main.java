e2eeca512c7327459c022a39ecb05c0a19901702
DulasiKeerthiratne <dulasi@keerthiratne.com>
2024-11-11 10:59:59 +0530

    feat(cli): implement initial input validation setup

--- CLI/src/Main.java
+++ CLI/src/Main.java
@@ -1,19 +1,43 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+class negativeInputError extends Exception {
+    public negativeInputError(String message) {
+        super(message);
+    }
+}
+
 public class Main {
+
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Enter total tickets available: ");
-        int totalTickets = scanner.nextInt();
+        int totalTickets = validate("Enter total tickets available: ");
+        int ticketReleaseRate = validate("Enter ticket release rate: ");
+        int customerRetrievalRate = validate("Enter customer retrieval rate: ");
+        int maxTicketCapacity = validate("Enter max ticket capacity: ");
+    }
 
-        System.out.print("Enter ticket release rate: ");
-        int ticketReleaseRate = scanner.nextInt();
+    public static int validate(String text) {
+        int num = 0;
+        while (true) {
+            try {
+                System.out.print(text);
+                num = scanner.nextInt();
 
-        System.out.print("Enter customer retrieval rate: ");
-        int customerRetrievalRate = scanner.nextInt();
-
-        System.out.print("Enter max ticket capacity: ");
-        int maxTicketCapacity = scanner.nextInt();
+                if (num < 0) {
+                    throw new negativeInputError("Ticket count cannot be negative");
+                } else {
+                    System.out.println();
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println( "Invalid input: Please enter an integer \n");
+                scanner.next();
+            } catch (negativeInputError e) {
+                System.out.println( "Invalid input: Please enter an positive integer\n");
+            }
+        }
+        return num;
     }
 }
