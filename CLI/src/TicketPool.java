52d3e2fdd548d9bd2d9152d8e501a77c9d633690
DulasiKeerthiratne <dulasi@keerthiratne.com>
2024-12-08 09:22:23 +0530

    refactor(cli): improve Vendor class to handle ticket addition without assigning a fixed ticket count to vendor

--- CLI/src/TicketPool.java
+++ CLI/src/TicketPool.java
@@ -1,35 +1,83 @@
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class TicketPool {
-    private int ticketsInPool; //current number of tickets in pool
+    int totalTickets;
+    private int maxTicketCapacity;
+    private List<Ticket> ticketPool = Collections.synchronizedList(new ArrayList<>());
 
     public TicketPool() {
     }
 
-    public TicketPool(int totalTickets, int ticketsInPool) {
-        this.ticketsInPool = ticketsInPool;
+    public TicketPool(int totalTickets, int maxTicketCapacity) {
+        this.totalTickets = totalTickets;
+        this.maxTicketCapacity = maxTicketCapacity;
     }
 
-    public int getTicketsInPool() {
-        return ticketsInPool;
+    public synchronized int getTotalTickets() {
+        return totalTickets;
     }
 
-    public void setTicketsInPool(int ticketsInPool) {
-        this.ticketsInPool = ticketsInPool;
+    public void setTotalTickets(int totalTickets) {
+        this.totalTickets = totalTickets;
     }
 
-    @Override
-    public String toString() {
-        return "TicketPool{" +
-                ", ticketsInPool=" + ticketsInPool +
-                '}';
+    public int getMaxTicketCapacity() {
+        return maxTicketCapacity;
     }
 
-    public synchronized void addTicket(){
-        this.ticketsInPool++;
-        System.out.println("Ticket sold by " + Thread.currentThread().getName() + " - Ticket Pool has " + getTicketsInPool() + " Ticket(s)");
+    public void setMaxTicketCapacity(int maxTicketCapacity) {
+        this.maxTicketCapacity = maxTicketCapacity;
     }
 
-    public synchronized void removeTicket(int customerNumber){
-        this.ticketsInPool--;
-        System.out.println("Ticket bought by " + Thread.currentThread().getName() + " - Ticket Pool has " + getTicketsInPool() + " Ticket(s)");
+    public List<Ticket> getTicketPool() {
+        return ticketPool;
+    }
+
+    public void setTicketPool(List<Ticket> ticketPool) {
+        this.ticketPool = ticketPool;
+    }
+
+    public String dateTime() {
+        Date date = new Date();
+        SimpleDateFormat day = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat time = new SimpleDateFormat("hh:mm");
+        String sellTime = day.format(date) + " at " + time.format(date);
+        return sellTime;
+    }
+
+    public synchronized void addTicket() {
+        while (ticketPool.size() > maxTicketCapacity) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        Ticket vendorTicket = new Ticket("Concert Ticket", 50000, Thread.currentThread().getName(), dateTime());
+        this.ticketPool.add(vendorTicket);
+        this.totalTickets--;
+        System.out.println("\nTicket sold by " + Thread.currentThread().getName() + ": Ticket Pool has " + ticketPool.size() + " Ticket(s)");
+        notifyAll();
+    }
+
+    public synchronized void removeTicket() {
+        while (ticketPool.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        // Once a ticket is available, proceed with the purchase
+        Ticket customerTicket = ticketPool.remove(0);
+        customerTicket.setTicketCustomer(Thread.currentThread().getName());
+        customerTicket.setSellTime(dateTime());
+        System.out.println("\nTicket bought by " + Thread.currentThread().getName() + ": Ticket Pool has " + ticketPool.size() + " Ticket(s)" +
+                "\n Ticket bought: " + customerTicket.toString());
+        notifyAll();
     }
 }
--- CLI/src/Vendor.java
+++ CLI/src/Vendor.java
@@ -1,23 +1,78 @@
+/**
+ * The Vendor class represents a vendor in the ticketing system.
+ *
+ * @author Dulasi
+ */
 public class Vendor implements Runnable {
-    private int totalTickets;
-    private int ticketReleaseRate;
+    private int vendorId;           // Unique identifier for the vendor
+    private int ticketReleaseRate;  // Time interval (in seconds) for releasing tickets
     private TicketPool ticketPool;
+    private int vendorNumber;// Shared ticket pool
 
+
+    //Default constructor for the Vendor class.
     public Vendor() {
     }
 
-    public Vendor(int totalTickets, int ticketReleaseRate, TicketPool ticketPool) {
-        this.totalTickets = totalTickets;
+    /**
+     * Parameterized constructor for the Vendor class.
+     *
+     * @param ticketReleaseRate  - Time interval (in seconds) for releasing tickets.
+     * @param ticketPool         - Shared ticket pool.
+     */
+    public Vendor(int vendorNumber, int ticketReleaseRate, TicketPool ticketPool) {
+        this.vendorNumber = vendorNumber;
         this.ticketReleaseRate = ticketReleaseRate;
         this.ticketPool = ticketPool;
     }
 
+    // Getter and Setter methods
+    public int getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(int vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public int getTicketReleaseRate() {
+        return ticketReleaseRate;
+    }
+
+    public void setTicketReleaseRate(int ticketReleaseRate) {
+        this.ticketReleaseRate = ticketReleaseRate;
+    }
+
+    public TicketPool getTicketPool() {
+        return ticketPool;
+    }
+
+    public void setTicketPool(TicketPool ticketPool) {
+        this.ticketPool = ticketPool;
+    }
+
+    /**
+     * method for string representation of the vendor object.
+     *
+     * @return String representation of the vendor object.
+     */
+    @Override
+    public String toString() {
+        return "Vendor{" +
+                "vendorId=" + vendorId +
+                ", ticketReleaseRate=" + ticketReleaseRate +
+                ", ticketPool=" + ticketPool +
+                '}';
+    }
+
+    //vendor thread
     @Override
     public void run() {
-        for (int i = 0; i < totalTickets/5; i++) {
+        while (ticketPool.getTotalTickets() >= vendorNumber) {
             ticketPool.addTicket();
+
             try {
-                Thread.sleep(ticketReleaseRate * 1000);
+                Thread.sleep(ticketReleaseRate * 1000); // Simulate time interval between ticket releases
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
